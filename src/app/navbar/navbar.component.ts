905e02de980be45975549a57e35395457cf45a1a
Ema <ema0kd@gmail.com>
2023-06-15 15:11:58 +0200

    finished navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Collapse, Dropdown, initTE } from 'tw-elements';
 
+initTE({ Collapse, Dropdown });
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    initTE({ Collapse, Dropdown });
+  }
 
 }
